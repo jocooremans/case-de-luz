d94523dd867d09ccb565f9d0a81f163e8b95e431
jocooremans <rencobvba@gmail.com>
2025-06-04 12:10:56 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Welkom bij VICO Casa Luz</h1>
+      <p>Een off-grid retreat woning op Ibiza, verbonden met de natuur.</p>
+    </div>
+  )
+}
